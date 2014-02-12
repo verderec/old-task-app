9670f9862b2d541bfa3830012df4bb25fb2a843f
Scotty Logan <swl@stanford.edu>
2014-02-11 18:03:42 -0800

    added node support

---
+++ node-tasklist.js
@@ -0,0 +1,8 @@
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require('node-localstorage').LocalStorage;
+  localStorage = new LocalStorage('./scratch');
+}
+
+eval(fs.readFileSync('./tasklist.js', 'utf8'));
+
+module.exports = taskList;
