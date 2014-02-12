36a6ecbccbdc7011a5cc156a66a032541d099bb5
Scotty Logan <swl@stanford.edu>
2014-02-11 18:27:08 -0800

    fixed missing fs require in node-tasklist

--- node-tasklist.js
+++ node-tasklist.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 if (typeof localStorage === "undefined" || localStorage === null) {
   var LocalStorage = require('node-localstorage').LocalStorage;
   localStorage = new LocalStorage('./scratch');
