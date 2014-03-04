1b84935d043ed45ed78db9915963c7fb73310b05
Scotty Logan <swl@stanford.edu>
2014-03-04 15:15:16 -0800

    initial implementation of taskApp class

--- js/tasklist.js
+++ js/tasklist.js
@@ -46,6 +46,109 @@ var
 
     return that;
   },
+  
+  /**
+   * TaskApp representation
+   *
+   * @constructor
+   * @param {String} name - name for this task app
+   */
+  taskApp = function taskApp (name) {
+    var appName = name || 'Task App',
+        taskLists = [],
+        taskListsByName = {},
+
+        that = {
+          
+          /**
+           * find - find a task list by name
+           * @param {String} name - name of the task list
+           */
+          find: function find (name) {
+            var idx = taskListsByName[name];
+            return (typeof idx === 'number') ? taskLists[idx] : null;
+          },
+
+          /**
+           * add - add a new task list
+           */
+          add: function add (name) {
+            if (find(name)) {
+              return null; // should throw an error
+            } else {
+              taskLists.push(taskList(name));
+              taskListsByName[name] = taskLists.length - 1;
+              return taskListsByName[name];
+            }
+            return null;
+          },
+          
+          /**
+           * remove - remove a task list
+           */
+          remove: function remove (name) {
+            var idx = taskListsByName[name];
+
+            if (typeof idx === 'number') {
+              delete taskLists[idx];
+              return true;
+            }
+            return false;
+          },
+          
+          /**
+           * forEach - call a function on each TaskList
+           */
+          forEach: function forEach (fn) {
+            return taskLists.forEach(fn);
+          },
+
+          /**
+           * save - save the Task App state by saving task list names
+           */
+          save: function save () {
+            var tmpLists = taskLists.map(function (list) {
+              return list.getName();
+            });
+            localStorage.setItem(appName + ' [VERSION]', 1);
+            localStorage.setItem(appName, JSON.stringify(tmpLists));
+          },
+
+          /**
+           * load - load the Task App state
+           */
+          load: function load () {
+            var localItems = localStorage.getItem(appName),
+            tmpLists = localItems ? JSON.parse(localItems) : null,
+            storageVersion = localStorage.getItem(appName + ' [VERSION]');
+
+            if (tmpLists) {
+              // storageVersion doesn't really matter at this time
+              // since the task constructor is flexible,
+              // but here's how different versions could be handled
+              switch (storageVersion) {
+
+                case 1:
+                case '1':
+                  // just an array of list names
+                  // FALLTHROUGH
+                  tmpLists.forEach(function (listName) {
+                    taskLists.add(listName);
+                  });
+                  break;
+
+                default:
+                  throw new Error('Unknown task storage version')
+                  break;
+              }
+            }
+          }
+        };
+
+    that.load();
+    return that;
+
+  },
 
   /**
    * TaskList representation
@@ -56,147 +159,144 @@ var
   taskList = function taskList (name) {
 
     var listName = name || 'Task List',
+        id = null,
+        tasks = [],
+        tasksById = {},
+        that = {
 
-    tasks = [],
-
-    tasksById = {},
-
-    that = {
-
-      /**
-       * clearAll - empty the task lsit
-       */
-      clearAll: function clearAll () {
-        localStorage.clear();
-        tasks = [];
-      },
+          /**
+           * clearAll - empty the task lsit
+           */
+          clearAll: function clearAll () {
+            localStorage.clear();
+            tasks = [];
+          },
       
-      /**
-       * add - add a new task to the list
-       * 
-       * @param {Object} spec - object spec for the new task
-       * @returns {String} task ID
-       */
-      
-      add: function add (spec) {
-        var newTask = task(spec);
-        tasks.push(newTask);
-        tasksById[newTask.id] = tasks.length - 1;
-        that.save();
-        return newTask.id;
-      },
+          /**
+           * add - add a new task to the list
+           * 
+           * @param {Object} spec - object spec for the new task
+           * @returns {String} task ID
+           */
+          add: function add (spec) {
+            var newTask = task(spec);
+            tasks.push(newTask);
+            tasksById[newTask.id] = tasks.length - 1;
+            that.save();
+            return newTask.id;
+          },
 
-      /**
-       * remove - remove a task from the list
-       * 
-       * @param {String} taskId - ID of the task to remove
-       * @returns {Boolean} - true if the task was removed
-       */
-      remove: function remove (taskId) {
-        var idx = tasksById[taskId],
-        removed = 0;
-        if (idx !== null) {
-          delete tasksById[taskId];
-          removed = tasks.splice(tasksById[taskId], 1).length;
-          that.save();
-        }
+          /**
+           * remove - remove a task from the list
+           * 
+           * @param {String} taskId - ID of the task to remove
+           * @returns {Boolean} - true if the task was removed
+           */
+          remove: function remove (taskId) {
+            var idx = tasksById[taskId],
+            removed = 0;
+            if (idx !== null) {
+              delete tasksById[taskId];
+              removed = tasks.splice(tasksById[taskId], 1).length;
+              that.save();
+            }
 
-        return (removed === 1);
-      },
+            return (removed === 1);
+          },
 
-      /**
-       * get - find a task by task ID
-       * 
-       * @param {String} taskId - ID of the task to remove
-       * @returns {Object} - the task, or null
-       */
-      get: function get (taskId) {
-        return tasks[tasksById[taskId]];
-      },
+          /**
+           * get - find a task by task ID
+           * 
+           * @param {String} taskId - ID of the task to remove
+           * @returns {Object} - the task, or null
+           */
+          get: function get (taskId) {
+            return tasks[tasksById[taskId]];
+          },
 
-      getAll: function getAll () {
-        return tasks;
-      },
+          getAll: function getAll () {
+            return tasks;
+          },
 
-      getName: function getName () {
-        return listName;
-      },
+          getName: function getName () {
+            return listName;
+          },
 
-      forEach: function forEach (fn) {
-        return tasks.forEach(fn);
-      },
+          forEach: function forEach (fn) {
+            return tasks.forEach(fn);
+          },
 
-      find: function find (nameOrId) {
-        var idx = tasksById[nameOrId],
-        matches = [];
+          find: function find (nameOrId) {
+            var idx = tasksById[nameOrId],
+            matches = [];
 
-        if (!idx) {
-          matches = tasks.filter(function (t) { return (t.name === nameOrId); });
-        } else {
-          matches.push(tasks[idx]);
-        }
-        return matches.length > 0 ? matches[0] : null;
-      },
+            if (!idx) {
+              matches = tasks.filter(function (t) { return (t.name === nameOrId); });
+            } else {
+              matches.push(tasks[idx]);
+            }
+            return matches.length > 0 ? matches[0] : null;
+          },
 
-      search: function search (pattern, fields, flags) {
-        if (typeof fields === 'string') {
-          flags = fields;
-          fields = [ 'name', 'details' ];
-        }
-        flags = flags || 'i';
+          search: function search (pattern, fields, flags) {
+            if (typeof fields === 'string') {
+              flags = fields;
+              fields = [ 'name', 'details' ];
+            }
+            flags = flags || 'i';
 
-        if (typeof pattern === 'string') {
-          pattern = new RegExp(pattern, flags);
-        }
-        if (pattern instanceof RegExp) {
-          return tasks.filter(function (t) {
-            fields.some(function (field) {
-              return pattern.test(t[field]);
-            });
-          });
-        }
-        return [];
-      },
+            if (typeof pattern === 'string') {
+              pattern = new RegExp(pattern, flags);
+            }
+            if (pattern instanceof RegExp) {
+              return tasks.filter(function (t) {
+                fields.some(function (field) {
+                  return pattern.test(t[field]);
+                });
+              });
+            }
+            return [];
+          },
 
-      save: function save () {
-        localStorage.setItem(listName + ' [VERSION]', 2);
-        localStorage.setItem(listName, JSON.stringify(tasks));
-      },
+          save: function save () {
+            localStorage.setItem(listName + ' [VERSION]', 2);
+            localStorage.setItem(listName, JSON.stringify(tasks));
+          },
 
-      load: function load () {
-        var localItems = localStorage.getItem(listName),
-        tmpTasks = localItems ? JSON.parse(localItems) : null,
-        storageVersion = localStorage.getItem(listName + ' [VERSION]');
+          load: function load () {
+            var localItems = localStorage.getItem(listName),
+            tmpTasks = localItems ? JSON.parse(localItems) : null,
+            storageVersion = localStorage.getItem(listName + ' [VERSION]');
 
-        if (tmpTasks) {
-          // storageVersion doesn't really matter at this time
-          // since the task constructor is flexible,
-          // but here's how different versions could be handled
-          switch (storageVersion) {
+            if (tmpTasks) {
+              // storageVersion doesn't really matter at this time
+              // since the task constructor is flexible,
+              // but here's how different versions could be handled
+              switch (storageVersion) {
 
-          case null:
-            // original, names only, no id
-            // FALLTHROUGH
-          case 1:
-          case '1':
-            // names and details, no id
-            // FALLTHROUGH
+              case null:
+                // original, names only, no id
+                // FALLTHROUGH
+              case 1:
+              case '1':
+                // names and details, no id
+                // FALLTHROUGH
 
-          case 2:
-          case '2':
-            // names, id, other fields
-            tmpTasks.forEach(function (t) {
-              that.add(t);
-            });
-            break;
+              case 2:
+              case '2':
+                // names, id, other fields
+                tmpTasks.forEach(function (t) {
+                  that.add(t);
+                });
+                break;
 
-          default:
-            throw new Error('Unknown task storage version')
-            break;
+              default:
+                throw new Error('Unknown task storage version')
+                break;
+              }
+            }
           }
-        }
-      }
-    };
+        };
 
     that.load();
 
