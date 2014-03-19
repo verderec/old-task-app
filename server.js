37525b816da4eeb35bb38c004b70a3ffa5f67275
Scotty Logan <swl@stanford.edu>
2014-03-18 19:01:56 -0700

    added static node http server

---
+++ server.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+var static = require('node-static');
+
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new static.Server();
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    }).resume();
+}).listen(8080);
